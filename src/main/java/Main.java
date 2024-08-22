21feaf89cd58a774825e3930b50fd55aa1431bef
Dmitriy Shirokov <shirokov.d@autodoc.ru>
2024-08-22 19:10:24 +0300

    Fixes: one more input check and patterns for better outputs.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,19 +12,23 @@ public class Main {
         scanner.useDelimiter("\n"); //Чтобы scanner.next() читал не только до пробела, а всю строку (Aston Martin, Alfa Romeo, Ford Mustang)
         ArrayList<Car> participants = new ArrayList<>();
 
+        String anotherNameMessage = ", назовите по-другому:";
+        String outOfSpeedRangeMessage = "Введите значение от 1 до 250:";
+
         System.out.println("Приветствуем вас на нашей гонке! Сегодня у нас трое участников, и совсем скоро мы узнаем победителя!");
 
+        //Главный цикл добавления участников
         for (int i = 0; i < 3; i++) {
             int position = i + 1;
 
-            System.out.println("Введите автомобиль либо имя " + position + "го участника:");
-
+            //Блок ввода имени
+            System.out.println(String.format("Введите автомобиль либо имя %dго участника:", position));
             String inputName;
             //При одинаковой скорости будет несколько победителей. Чтобы их различать, решил сделать имя уникальным.
             while (true) {
                 inputName = scanner.next().trim();  //Пробелы по краям уникальным имя не делают, перепутать таких участников очень просто.
                 if (inputName.isEmpty())
-                    System.out.println("Имя не может быть пустым или состоять из пробелов.\nВведите автомобиль либо имя " + position + "го участника:");
+                    System.out.println("Имя не может быть пустым или состоять из пробелов" + anotherNameMessage);
                 else {
                     boolean isSame = false;
                     for (Car current : participants) {
@@ -34,27 +38,29 @@ public class Main {
                         }
                     }
                     if (!isSame) break;
-                    else {
-                        System.out.println("Такой участник уже есть, назовите по-другому:");
-                    }
+                    else System.out.println("Такой участник уже есть" + anotherNameMessage);
                 }
             }
 
+            //Блок ввода скорости
             System.out.println("Введите скорость " + position + "го участника:");
-
             int inputSpeed;
             //Проверка скорости на int и на соответствие диапазону
             while (true) {
                 if (scanner.hasNextInt()) {
                     inputSpeed = scanner.nextInt();
                     if (inputSpeed > 0 && inputSpeed <= 250) break;
-                    else System.out.println("Ведите число от 1 до 250:");
+                    else System.out.println(outOfSpeedRangeMessage);
                 } else {
-                    System.out.println("Ведите целое число от 1 до 250 без любых других символов:");
-                    scanner.next();
+                    String text = scanner.next();
+                    if (text.matches("[0-9]+")) //На случай ввода числа за пределами int: 97314897227297497983024
+                        System.out.println(outOfSpeedRangeMessage);
+                    else
+                        System.out.println("Ведите целое число без любых других символов:");   //Всё остальное: 250 км/ч, 123.4 и т.п.
                 }
             }
 
+            //Справились с вводом, можно добавлять учатника
             participants.add(new Car(inputName, inputSpeed));
 
         }
--- src/main/java/entities/Race.java
+++ src/main/java/entities/Race.java
@@ -22,16 +22,26 @@ public class Race {
 
         //Форматирование вывода
         if (participants.size() == 1) {
-            return "И нашим победителем становится " + participants.get(0).name + "!";
+            return "И нашим победителем становится " + participants.get(0).name + " с результатом " + formatDistance(maxDistance) + "!";
         } else {
-            StringBuilder result = new StringBuilder();
+            StringBuilder winners = new StringBuilder();
             for (int i = 0; i < participants.size(); i++) {
-                result.append(participants.get(i).name);
+                winners.append(participants.get(i).name);
                 //Если победителей несколько, их надо разделить запятыми, а двух последних союзом и
-                if (i == participants.size() - 2) result.append(" и ");
-                else if (i != participants.size() - 1) result.append(", ");
+                if (i == participants.size() - 2) winners.append(" и ");
+                else if (i != participants.size() - 1) winners.append(", ");
             }
-            return "Невероятно, но " + result + " финишировали с одинаковым результатом!";
+            return "Невероятно, но проехав по " + formatDistance(maxDistance) + ", " + winners + " финишировали с одинаковым результатом!";
         }
+    }
+
+    //Для более красивого вывода расстояния и просто чтобы switch тоже использовать
+    String formatDistance(int distance) {
+        String correctWord = switch (distance % 10) {
+            case 1 -> "километр";
+            case 2, 3, 4 -> "километра";
+            default -> "километров";
+        };
+        return distance + " " + correctWord;
     }
 }
